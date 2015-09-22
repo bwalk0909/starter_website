37deb667c1c02c618a779db273431cc0c1140cce
bwalk0909 <bwalk0909@gmail.com>
2015-09-22 18:39:04 -0400

    establish my first repository

--- js/node_only.js
+++ js/node_only.js
@@ -2,3 +2,17 @@
    Node only scripts - Only to be used via node command line tool
    				     - this file is NOT included in index.html
    ========================================================================== */
+
+    var game= "*********\nDungeons and Dragons!\n*********"
+    console.log(game);
+
+    var message = "Dungeons and Dragons!"
+    var name = "Brian"
+    var profession = "Developer"
+    console.log(message + ' my name is ' + name + ' and I am a ' + profession + '\n*********');
+
+
+    var currently = "Cascade User Conference"
+    var location = "Atlanta"
+    var purpose = "Developer"
+    console.log('Today, I am attending the ' + currently + ' in  ' + location + ' in hopes of becoming a better ' + profession + '\n*********');
